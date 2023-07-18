09307279c315bf458d847489659b608ad7567e0d
Zhang0805mile <102213636+Zhang0805mile@users.noreply.github.com>
2023-07-18 10:50:44 +0800

    return-0-noglibc.s

--- src/demos/return-0-noglibc.s
+++ src/demos/return-0-noglibc.s
@@ -1,7 +1,9 @@
 .globl _start
 	
 .text
-_start:
-	movl $1, %eax
-	movl $0, %ebx # return value
+_start:// 程序的入口
+	movl $1, %eax // 系统调用号为1，表示退出
+	movl $0, %ebx # return value //递给exit系统调用的返回值存储在%ebx寄存器中，
+ //因此将其设置为0表示正常退出。
 	int $0x80
+// 使用int $0x80 执行syscall；
