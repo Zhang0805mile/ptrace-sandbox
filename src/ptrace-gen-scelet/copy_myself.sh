1357ac8e78b61780f410ed52baf86665ca4f1277
Zhang0805mile <102213636+Zhang0805mile@users.noreply.github.com>
2023-07-18 11:02:46 +0800

    copy_myself.sh

--- src/ptrace-gen-scelet/copy_myself.sh
+++ src/ptrace-gen-scelet/copy_myself.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
-
+//这段脚本的作用是检查指定目录中的文件，并根据需要将文件复制到另一个目录中。
 if [ ! -d src/ptrace-gen ]; then
     mkdir ./src/ptrace-gen
 fi
 if [ ! -d obj/ptrace-gen ]; then
     mkdir -p ./obj/ptrace-gen
 fi
+//for循环，用于遍历目录./src/ptrace-gen-scelet下的所有文件（不包括copy_myself.sh文件）。对于每个文件，首先获取其文件名（不包含路径）保存到file变量中。
 
 for full_file in $(find ./src/ptrace-gen-scelet -type f | grep -v copy_myself.sh); do
     file=$(basename $full_file)
